d95015ec9ec9cf3fe68b75aa99614888de87d281
Alina <berens.alina019@gmail.com>
2020-10-08 14:08:15 +0300

    Retrofit, problem with response

--- app/src/main/java/ru/padawans/moviesapp/LoginActivity.kt
+++ app/src/main/java/ru/padawans/moviesapp/LoginActivity.kt
@@ -21,26 +21,26 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-    private fun signin(username: String, password: String, request_token: String) {
+//    private fun signin(username: String, password: String, request_token: String) {
+//
+//        val retIn =
+//            RetrofitHTTPconnection.getRetrofitInstance().create(UserLoginInterface::class.java)
+//        val signInInfo = User(username, password, request_token)
+//
+//        retIn.getToken(signInInfo).enqueue(object : retrofit2.Callback<LoginResponse> {
+//            override fun onResponse(
+//                call: retrofit2.Call<LoginResponse>,
+//                response: Response<LoginResponse>
+//            ) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onFailure(call: retrofit2.Call<LoginResponse>, t: Throwable) {
+//                TODO("Not yet implemented")
+//            }
+//        })
 
-        val retIn =
-            RetrofitHTTPconnection.getRetrofitInstance().create(UserLoginInterface::class.java)
-        val signInInfo = User(username, password, request_token)
-
-        retIn.getToken(signInInfo).enqueue(object : retrofit2.Callback<LoginResponse> {
-            override fun onResponse(
-                call: retrofit2.Call<LoginResponse>,
-                response: Response<LoginResponse>
-            ) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onFailure(call: retrofit2.Call<LoginResponse>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
-
-    }}
+    }
 
 
 
